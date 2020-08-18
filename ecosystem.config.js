5476a977f73f3938271429bed3e3cee157a40920
xiaoming.bai <80583600@qq.com>
2020-08-18 17:13:09 +0800

    How to deploy using PM2 cluster mode? https://nuxtjs.org/faq/deployment-pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+// How to deploy using PM2 cluster mode?
+// https://nuxtjs.org/faq/deployment-pm2
+
+module.exports = {
+  apps: [
+    {
+      name: 'NuxtAppName',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+    },
+  ],
+}
